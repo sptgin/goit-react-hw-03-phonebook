aa92c931246b243fb8fd6d178bded42b5248d66f
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-10-18 22:41:32 +0300

    make use LocalStorage functionality

--- src/App.js
+++ src/App.js
@@ -7,14 +7,24 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
+
+  componentDidMount() {
+    console.log(`MOUNT`);
+    const localContacts = localStorage.getItem("contacts");
+    const parseContacts = JSON.parse(localContacts);
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log(`UPDATE`);
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   getChange = (event) => {
     const { name, value } = event.currentTarget;
